5dc93389ae2f67b4b5600e6f174db40a13a06ccf
n-tamori <n-tamori@creationline.com>
2020-06-20 17:31:41 +0900

    Expressからデータベースをゴニョゴニョできるようにした

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+
+  const client = require("../db_client").pg_client()
+
+  let sql_result = 'None'
+  // client.connect()
+  //   .then(() => console.log("Connected successfuly"))
+  //   .then(() => client.query("select * from chat"))
+  //   .then(results => 
+  //     console.table(results.rows)
+  //   )
+  //   .catch((e => console.log(e)))
+  //   .finally((() => client.end()))
+  client.connect()
+    .then(() => console.log("Connected successfuly"))
+    .then(() => client.query("select * from chat"))
+    .then(function (results) {
+      console.table(results.rows)
+      res.render('index', { title: 'セレクト', sql_result: JSON.stringify(results.rows)})
+    })
+    .catch((e => console.log(e)))
+    .finally((() => client.end()))
+
+  // res.render('index', { title: 'Express' ,sql_result: results.rows});
+
+
 });
 
 module.exports = router;
---
+++ app/routes/insert.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+
+  let msg = req.query.msg || "ふにょふにょ"
+  const client = require("../db_client").pg_client()
+
+  client.connect()
+    .then(() => console.log("Connected successfuly"))
+    .then(() => client.query(`INSERT INTO chat (timestamp, comment) VALUES( now(), '${msg}' );`))
+    .then(() => client.query(`SELECT * FROM chat;` ))
+    .then(function (results) {
+      console.table(results.rows)
+      res.render('index', { title: 'インサート', sql_result: JSON.stringify(results.rows) })
+    })
+    .catch((e => console.log(e)))
+    .finally((() => client.end()))
+
+
+  // res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
