eabb1dc4e018044d18fdf0f7d8b42d49eee709bf
n-tamori <n-tamori@creationline.com>
2020-06-20 15:23:36 +0900

    ダミーデータの作成

---
+++ init-sql/00-test.sql
@@ -0,0 +1,8 @@
+CREATE Table chat (
+  timestamp timestamp, 
+  comment text
+);
+
+INSERT INTO chat (timestamp ,comment) VALUES (now() ,'ダミー：いいいいい');
+
+INSERT INTO chat (timestamp ,comment) VALUES (now() ,'ダミー：ううううう');
