4a147cad2e137c6e2ec12e97083fa09ca4838879
LostGeneration1999 <masaya4426saganosen@gmail.com>
2020-09-22 18:02:47 +0900

    修正

--- init-sql/00-test.sql
+++ init-sql/00-test.sql
@@ -2,6 +2,3 @@ CREATE Table chat (
   timestamp timestamp, 
   comment text
 );
-
-INSERT INTO chat (timestamp ,comment) VALUES (now() ,'ダミー：いいいいい');
-INSERT INTO chat (timestamp ,comment) VALUES (now() ,'ダミー：ううううう');
